2de34a1b71c1be9f75ce42ebb51776a45e0c0c10
Rich Edmunds <xBLADEx@users.noreply.github.com>
2023-12-28 20:15:07 +0000

    fix: Add localStorage support

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,51 +1,46 @@
 import { tiles } from './tiles.js';
 
 const letters = ['J', 'I', 'N', 'G', 'O'];
-
-const possibilities = [];
-console.log('possibilities', possibilities);
-const history = [];
-
-letters.forEach((letter) => {
-  tiles.forEach((tile) => {
-    possibilities.push({
-      ...tile,
-      id: `${tile.id}${letter}`,
-      letter: letter,
-    });
-  });
-});
-
+const possibilities = JSON.parse(window.localStorage.getItem('possibilities')) || [];
+const history = JSON.parse(window.localStorage.getItem('history')) || [];
+let totalPossibilities = possibilities.length + history.length;
 const draw = document.querySelector('#js-draw');
 const tileResult = document.querySelector('#js-tile');
 const historyElement = document.querySelector('#js-history');
-const totalPossibilities = possibilities.length;
 
-draw.addEventListener('click', () => {
-  const remaining = possibilities.length;
-  if (remaining === 0) {
+function generateTiles() {
+  letters.forEach((letter) => {
+    tiles.forEach((tile) => {
+      possibilities.push({
+        ...tile,
+        id: `${tile.id}${letter}`,
+        letter,
+      });
+    });
+  });
+
+  window.localStorage.removeItem('possibilities');
+  window.localStorage.removeItem('history');
+  window.localStorage.setItem('possibilities', JSON.stringify(possibilities));
+  totalPossibilities = possibilities.length;
+}
+
+if (possibilities.length === 0 || history.length === 0) {
+  generateTiles();
+}
+
+
+function renderHistory() {
+  if (history.length === 0) {
     return;
   }
 
-  const randomIndex = Math.floor(Math.random() * remaining);
-  const tile = possibilities.splice(randomIndex, 1)[0];
-  history.push(tile);
-
-  const result = `
-    <span class="letter">${tile.letter}</span>
-    <img src="${tile.image}" alt="${tile.name}" class="image">
-    <div class="details">
-      <h2 class="name">${tile.name}</h2>
-      <p class="description">${tile.description}</p>
-    </div>
-  `;
-
-  tileResult.innerHTML = result;
+  const remaining = possibilities.length;
 
   const historyResult = `
     <header>
       <h2>History</h2>
-      <p>${remaining - 1} of ${totalPossibilities}</p>
+      <p>${remaining} of ${totalPossibilities}</p>
     </header>
     <ul class="history-list">${history
       .map((tile) => {
@@ -59,4 +54,40 @@ draw.addEventListener('click', () => {
   `;
 
   historyElement.innerHTML = historyResult;
+}
+
+renderHistory();
+
+draw.addEventListener('click', () => {
+  const remaining = possibilities.length;
+  if (remaining === 0) {
+    window.localStorage.removeItem('history');
+    window.localStorage.removeItem('possibilities');
+    const startText = draw.textContent;
+    draw.textContent = 'New Game';
+    historyElement.innerHTML = '';
+    tileResult.innerHTML = '';
+    generateTiles();
+    draw.textContent = startText;
+    return;
+  }
+
+  const randomIndex = Math.floor(Math.random() * remaining);
+  const tile = possibilities.splice(randomIndex, 1)[0];
+  history.push(tile);
+  window.localStorage.setItem('possibilities', JSON.stringify(possibilities));
+  window.localStorage.setItem('history', JSON.stringify(history));
+
+  const result = `
+    <span class="letter">${tile.letter}</span>
+    <img src="${tile.image}" alt="${tile.name}" class="image">
+    <div class="details">
+      <h2 class="name">${tile.name}</h2>
+      <p class="description">${tile.description}</p>
+    </div>
+  `;
+
+  tileResult.innerHTML = result;
+
+  renderHistory();
 });
